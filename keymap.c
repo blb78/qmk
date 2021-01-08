425edc9663d04c6d37ff8ad6cf8dde3a48f877dc
blb78 <blb78@protonmail.com>
2021-01-08 16:30:39 +0100

    add french keyboard

--- keymap.c
+++ keymap.c
@@ -1,100 +1,10 @@
 #include QMK_KEYBOARD_H
+#include "keymap_french.h"
 
 #define _QW 0
 #define _FN 1
-#define _VIM 2
+#define _ACCENT 2
 #define _NAV 3
-
-// French chars
-enum unicode_names {
-  E_AIG,
-  E_AIG_MAJ,
-  E_GRV,
-  E_GRV_MAJ,
-  E_CIRC,
-  E_CIRC_MAJ,
-  E_TREMA,
-  E_TREMA_MAJ,
-  A_CIRC,
-  A_CIRC_MAJ,
-  A_GRV,
-  A_GRV_MAJ,
-  C_CEDILLE,
-  C_CEDILLE_MAJ,
-  CARRE,
-  CUBE,
-  EURO,
-  BAHT,
-  COPYRIGHT,
-  REGISTERED,
-  OMEGA,
-  OMEGA_MAJ,
-  O_CIRC,
-  O_CIRC_MAJ,
-  U_GRAV,
-  U_GRAV_MAJ,
-  OE,
-  OE_MAJ,
-  PI,
-  DEGREE,
-  N_TILDE,
-  N_TILDE_MAJ,
-  MICRO,
-  SIGMA,
-  INFEQ,
-  SUPEQ,
-  GUILL_G,
-  GUILL_R,
-  UNEQUAL,
-  PRETTYMUCH,
-  INFINIT,
-};
-
-const uint32_t PROGMEM unicode_map[] = {
-  [E_AIG]  = 0x00E9,  // 0 é
-  [E_AIG_MAJ] = 0x00C9,  // 1 É
-  [E_GRV]  = 0x00E8,  // 2 è
-  [E_GRV_MAJ] = 0x00C8,  // 3 È
-  [E_CIRC]  = 0x00EA,  // 4 ê
-  [E_CIRC_MAJ] = 0x00CA,  // 5 Ê
-  [E_TREMA]  = 0x00EB,  // 6 ë
-  [E_TREMA_MAJ] = 0x00CB,  // 7 Ë
-  [A_CIRC]  = 0x00E2,  // 8 â
-  [A_CIRC_MAJ] = 0x00C2,  // 9 Ê
-  [A_GRV]  = 0x00E0,  // 10 à
-  [A_GRV_MAJ] = 0x00C0,  // 11 À
-  [C_CEDILLE]  = 0x00E7,  // 12 ç
-  [C_CEDILLE_MAJ] = 0x00C7,  // 13 Ç
-  [CARRE]  = 0x00B2,  // 14 ²
-  [CUBE] = 0x00B3,  // 15 ³
-  [EURO]  = 0x20AC,  // 16 €
-  [BAHT] = 0x0E3F,  // 17 ฿
-  [COPYRIGHT]  = 0x00A9,  // 18 ©
-  [REGISTERED] = 0x00AE,  // 19 ®
-  [OMEGA]  = 0x03C9,  // 20 ω
-  [OMEGA_MAJ] = 0x03A9,  // 21 Ω
-  [O_CIRC]  = 0x00F4,  // 22 ô
-  [O_CIRC_MAJ] = 0x00D4,  // 23 Ô
-  [U_GRAV]  = 0x00F9,  // 24 ù
-  [U_GRAV_MAJ] = 0x00D9,  // 25 Ù
-  [OE]  = 0x0153,  // 26 œ
-  [OE_MAJ] = 0x0152,  // 27 Œ
-  [PI] = 0x03C0, // 28 π
-  [DEGREE] = 0x00B0, // 29 °
-  [N_TILDE]  = 0x00F1,  // 30 ñ
-  [N_TILDE_MAJ] = 0x00D1,  // 31 Ñ
-  [MICRO]  = 0x00B5,  // 32 µ
-  [SIGMA] = 0x03C3,  // 33 σ
-  [INFEQ]  = 0x2264,  // 34 ≤
-  [SUPEQ] = 0x2265,  // 35 ≥
-  [GUILL_G]  = 0x00AB,  // 36 «
-  [GUILL_R] = 0x00BB,  // 37 »
-  [UNEQUAL]  = 0x2260,  // 38 ≠
-  [PRETTYMUCH] = 0x2248,  // 39 ≈
-  [INFINIT] = 0x221E,  // 40 ∞
-};
-
-#define CEDILLA XP(L_CEDILLA, U_CEDILLA)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -116,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(_FN), TO(_VIM), TO(_NAV), KC_RCTL
+		KC_LCTL, KC_LGUI, KC_LALT, LT(_ACCENT, KC_SPC), MO(_FN), TO(_ACCENT), TO(_NAV), KC_RCTL
       ),
 /* Fn
    * ,-----------------------------------------------------------------------------------------.
@@ -151,10 +61,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |       |       |                                   |      |       |       |       |
    * `-----------------------------------------------------------------------------------------'
    */
-	[_VIM] = LAYOUT_60_ansi(
+	[_ACCENT] = LAYOUT_60_ansi(
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-		TG(_VIM), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		TG(_ACCENT), KC_NO, KC_NO, FR_EACU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
       ),
@@ -187,8 +97,8 @@ uint32_t layer_state_set_user(uint32_t state) {
   case _FN:
     rgblight_sethsv_noeeprom(HSV_PINK);
     break;
-  case _VIM:
-    rgblight_sethsv_noeeprom(HSV_SPRINGGREEN);
+  case _ACCENT:
+    rgblight_sethsv_noeeprom(HSV_GREEN);
     break;
   case _NAV:
     rgblight_sethsv_noeeprom(HSV_RED);
